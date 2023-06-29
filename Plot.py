b8674f4c3eaa81d93e000aca8c246eecdcdb1f1b
Max Riffi-Aslett <uceiiff@ucl.ac.uk>
2023-06-29 19:17:15 +0000

    message

--- Plot.py
+++ Plot.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 
 def plot_df(df, window_size=7):
     # Convert PeriodIndex to DateTimeIndex if needed
@@ -21,6 +22,64 @@ def plot_df(df, window_size=7):
 
     plt.tight_layout()
     plt.show()
+
+
+def print_coherence(dic, topics_list,text):
+    from gensim.models.coherencemodel import CoherenceModel
+    from gensim.corpora.dictionary import Dictionary
+
+    # Coherence model
+    cm = CoherenceModel(topics=topics_list,
+                        texts=text,
+                        coherence='c_v',
+                        dictionary=dic)
+
+    coherence_per_topic = cm.get_coherence_per_topic()
+
+    topics_str = [','.join(t)[:45] for t in topics_list]
+    data_topic_score = zip(topics_str, coherence_per_topic)
+
+    for topic, coherence in data_topic_score:
+        print(f"Topic: {topic}\tCoherence: {coherence:.2f}")
+
+
+
+
+def plot_df_bootstrapped(df, window_size=7, indexes=None):
+    # Convert PeriodIndex to DateTimeIndex if needed
+    if isinstance(df.index, pd.PeriodIndex):
+        df.index = df.index.to_timestamp()
+
+    n = len(df.columns)
+    fig, axs = plt.subplots(n if indexes is None else len(indexes), 1, figsize=(10, 5*(n if indexes is None else len(indexes))))  # adjust the size based on number of plots
+
+    # Convert indexes to list if None is provided
+    if indexes is None:
+        indexes = list(range(n))
+
+    # Ensure indexes are within valid range
+    indexes = [i for i in indexes if i >= 0 and i < n]
+
+    # Check if axs is a single AxesSubplot or an array of AxesSubplots
+    if not isinstance(axs, np.ndarray):
+        axs = np.array([axs])
+
+    # Loop over specified indexes
+    for i, ax in zip(indexes, axs):
+        column = df.columns[i]
+
+        # Apply rolling mean
+        df_smoothed = df[column].rolling(window=window_size).mean()
+
+        ax.plot(df.index, df_smoothed)
+        ax.set_xlabel("Date")
+        ax.set_ylabel("Value")
+        ax.set_title(column)  # Set the title as the column name
+
+    plt.tight_layout()
+    plt.show()
+
+
 
 
 def print_coherence(dic, topics_list,text):
