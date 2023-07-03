69a3a1f1ee7d9794281424ade2114a77c86a8a4e
Max Riffi-Aslett <uceiiff@ucl.ac.uk>
2023-07-03 10:43:51 +0000

    Update

--- Plot.py
+++ Plot.py
@@ -1,38 +1,99 @@
-import pandas as pd
+import os
 import matplotlib.pyplot as plt
+import pandas as pd
 import numpy as np
 
-
-def plot_df(df, window_size=7):
+def plot_df(df, model, span_size=7, add_error_bars=False):
     # Convert PeriodIndex to DateTimeIndex if needed
     if isinstance(df.index, pd.PeriodIndex):
         df.index = df.index.to_timestamp()
 
-    n = len(df.columns)
-    fig, axs = plt.subplots(n, 1, figsize=(10, 5*n))  # adjust the size based on number of plots
+    if add_error_bars:
+        
+        save_directory = 'saved_plots_SE'
+        os.makedirs(save_directory, exist_ok=True)
+    else:
+            # Create the 'saved_plots' directory if it doesn't exist
+        save_directory = 'saved_plots'
+        os.makedirs(save_directory, exist_ok=True)
 
     # Loop over every column
     for i, column in enumerate(df.columns):
-        # Apply rolling mean
-        df_smoothed = df[column].rolling(window=window_size).mean()
+        # Apply moving average
+        df_smoothed = df[column].rolling(window=span_size).mean()
 
-        # Calculate standard error of the mean
-        sem = df[column].rolling(window=window_size).std() / np.sqrt(window_size)
+        fig, ax = plt.subplots(figsize=(10, 5))
+        ax.set_xlabel("Date in Months (From January 2012 to May 2023)")
+        ax.set_ylabel("Topic Proportion")
+        
+        # Construct the title with topic number and column name
+        title = f"{model}, Topic {i+1}: [{column}]"
+        
+        ax.set_title(title)
+    
+        if add_error_bars:
+            # Calculate standard errors
+            std_error = df[column].ewm(span=span_size).std() / np.sqrt(span_size)
+    
+            ax.fill_between(df.index, df_smoothed - std_error, df_smoothed + std_error, color='blue', alpha=0.3)
+            ax.plot(df.index, df_smoothed, label='Moving averages')
+            ax.plot([], [], color='blue', alpha=0.3, linewidth=10, label='Standard Error')
 
-        # Add standard error to the rolling mean
-        df_smoothed_plus_sem = df_smoothed + sem
-        df_smoothed_minus_sem = df_smoothed - sem
+            # Get the legend and update the handler for the error bar
+            legend = ax.legend()
+            legend.legendHandles[1].set_color('blue')
+            legend.legendHandles[1].set_alpha(0.3)
+            legend.get_texts()[1].set_color('blue')
 
-        axs[i].plot(df.index, df_smoothed, label='Rolling Mean')
-        axs[i].fill_between(df.index, df_smoothed_minus_sem, df_smoothed_plus_sem, color='b', alpha=0.2)
-        axs[i].set_xlabel("Date")
-        axs[i].set_ylabel("Value")
-        axs[i].set_title(column)  # Set the title as the column name
-        axs[i].legend()
+            save_filename = os.path.join(save_directory, f"topic_{i+1}_{model}_{column}_plot_with_SE.png")
+        else:
+            ax.plot(df.index, df_smoothed, label='Moving Average')
+            save_filename = os.path.join(save_directory, f"topic_{i+1}_{model}_{column}_plot.png")
+    
+        ax.legend()
+        plt.tight_layout()
+        fig.savefig(save_filename)
+        plt.close(fig)
 
+    # Save all plots in one image
+    fig, axs = plt.subplots(len(df.columns), 1, figsize=(10, 5 * len(df.columns)))  # adjust the size based on the number of plots
+    
+    for i, column in enumerate(df.columns):
+        # Apply moving average
+        df_smoothed = df[column].rolling(window=span_size).mean()
+
+        if add_error_bars:
+            # Calculate standard errors
+            std_error = df[column].ewm(span=span_size).std() / np.sqrt(span_size)
+    
+            axs[i].fill_between(df.index, df_smoothed - std_error, df_smoothed + std_error, color='blue', alpha=0.3)
+            axs[i].plot(df.index, df_smoothed, label='Moving averages')
+            axs[i].plot([], [], color='blue', alpha=0.3, linewidth=10, label='Standard Error')
+
+            # Get the legend and update the handler for the error bar
+            legend = axs[i].legend()
+            legend.legendHandles[1].set_color('blue')
+            legend.legendHandles[1].set_alpha(0.3)
+            legend.get_texts()[1].set_color('blue')
+        else:
+            axs[i].plot(df.index, df_smoothed, label='Moving averages')
+
+        axs[i].set_xlabel("Date in Months (From January 2012 to May 2023)")
+        axs[i].set_ylabel("Topic Proportion")
+        
+        # Set the title with topic number and column name
+        axs[i].set_title(f"{model}, Topic {i+1}: [{column}]")
+        
+    
     plt.tight_layout()
     plt.show()
-
+    # Display the combined plot
+    if add_error_bars:
+        save_filename_all = os.path.join(save_directory, f"{model}_all_plots_with_SE.png")
+    else:
+        save_filename_all = os.path.join(save_directory, f"{model}_all_plots.png")
+    fig.savefig(save_filename_all)
+    plt.close(fig)
 
 
 def print_coherence(dic, topics_list,text):
@@ -56,7 +117,7 @@ def print_coherence(dic, topics_list,text):
 
 
 
-def plot_df_bootstrapped(df, window_size=7, indexes=None):
+def plot_df_bootstrapped(df, span_size=7, indexes=None):
     # Convert PeriodIndex to DateTimeIndex if needed
     if isinstance(df.index, pd.PeriodIndex):
         df.index = df.index.to_timestamp()
@@ -79,8 +140,8 @@ def plot_df_bootstrapped(df, window_size=7, indexes=None):
     for i, ax in zip(indexes, axs):
         column = df.columns[i]
 
-        # Apply rolling mean
-        df_smoothed = df[column].rolling(window=window_size).mean()
+        # Apply exponential moving average
+        df_smoothed = df[column].ewm(span=span_size).mean()
 
         ax.plot(df.index, df_smoothed)
         ax.set_xlabel("Date")
@@ -93,22 +154,5 @@ def plot_df_bootstrapped(df, window_size=7, indexes=None):
 
 
 
-def print_coherence(dic, topics_list,text):
-    from gensim.models.coherencemodel import CoherenceModel
-    from gensim.corpora.dictionary import Dictionary
-
-    # Coherence model
-    cm = CoherenceModel(topics=topics_list,
-                        texts=text,
-                        coherence='c_v',
-                        dictionary=dic)
-
-    coherence_per_topic = cm.get_coherence_per_topic()
-
-    topics_str = [','.join(t)[:45] for t in topics_list]
-    data_topic_score = zip(topics_str, coherence_per_topic)
-
-    for topic, coherence in data_topic_score:
-        print(f"Topic: {topic}\tCoherence: {coherence:.2f}")
 
         
